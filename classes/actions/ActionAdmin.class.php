b62c78290defc7213a2044810091e57ba82b026e
Mzhelskiy Maxim <rus.engine@gmail.com>
2017-02-10 19:40:20 +0700

    small fix, version 2.0.0

--- classes/actions/ActionAdmin.class.php
+++ classes/actions/ActionAdmin.class.php
@@ -33,7 +33,7 @@ class PluginPage_ActionAdmin extends PluginAdmin_ActionPlugin
         $this->oAdminUrl = Engine::GetEntity('PluginAdmin_ModuleUi_EntityAdminUrl');
         $this->oAdminUrl->setPluginCode(Plugin::GetPluginCode($this));
         $this->oUserCurrent = $this->User_GetUserCurrent();
-        $this->Viewer_AppendScript(Plugin::GetWebPath(__CLASS__) . 'js/admin.js');
+        $this->Viewer_AppendScript(Plugin::GetWebPath(__CLASS__) . 'frontend/js/admin.js');
 
         $this->SetDefaultEvent('index');
     }
